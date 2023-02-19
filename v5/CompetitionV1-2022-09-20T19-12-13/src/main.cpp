2379d8bac5d73a6e4718a6f0435f9a479c245595
Arnav G <73305591+arnavg115@users.noreply.github.com>
2023-02-19 00:14:50 -0800

    drivetrain code

--- v5/CompetitionV1-2022-09-20T19-12-13/src/main.cpp
+++ v5/CompetitionV1-2022-09-20T19-12-13/src/main.cpp
@@ -13,9 +13,34 @@
 #include "vex.h"
 
 using namespace vex;
+extern controller Controller1;
+extern motor Left1;
+extern motor Left2;
+extern motor Right1;
+extern motor Right2;
 
-int main() {
+void driveControl()
+{
+  double speed = -Controller1.Axis3.value();
+  double turn = Controller1.Axis1.value();
+  double strafe = Controller1.Axis4.value();
+
+  Left1.spin(forward, speed + turn + strafe, pct);
+  Left2.spin(forward, speed + turn - strafe, pct);
+  Right1.spin(forward, speed - turn - strafe, pct);
+  Right2.spin(forward, speed - turn + strafe, pct);
+}
+
+void userControl()
+{
+  while (1)
+  {
+    driveControl();
+  }
+}
+
+int main()
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
 }
--- v5/CompetitionV1-2022-09-20T19-12-13/src/robot-config.cpp
+++ v5/CompetitionV1-2022-09-20T19-12-13/src/robot-config.cpp
@@ -4,12 +4,20 @@ using namespace vex;
 
 // A global instance of brain used for printing to the V5 brain screen
 brain Brain;
+controller Controller = controller(primary);
+motor Right1 = motor(PORT1, ratio18_1, false);
+motor Right2 = motor(PORT2, ratio18_1, false);
+motor Left1 = motor(PORT11, ratio18_1, true);
+motor Left2 = motor(PORT12, ratio18_1, true);
+
+bool RemoteControlCodeEnabled = true;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
  *
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
+void vexcodeInit(void)
+{
   // Nothing to initialize
 }
